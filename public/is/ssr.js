725643b5650950b1530d3cd08e5e09fac2226425
ezito developer <33319787+ezitoir@users.noreply.github.com>
2023-03-22 03:21:09 -0700

    Update ssr.js

--- public/is/ssr.js
+++ public/is/ssr.js
@@ -1,5 +1,20 @@
-'use strict';
-  
+'use strict'; 
+/** @license eziot-utils v1.0.0 
+ *
+ * Copyright (c) eziotir, Inc. and its affiliates. 
+ * 
+ */
+
 module.exports = function () { 
-    return typeof window === "undefined" && typeof document === "undefined" && typeof process !== "undefined"
+    return (
+        typeof window === "undefined" 
+        &&
+        typeof document === "undefined" 
+        && 
+        typeof process !== "undefined"
+        &&
+        typeof global !== "undefined"
+        &&
+        typeof localStorage === "undefined" 
+    )
 }
