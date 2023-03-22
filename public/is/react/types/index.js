767dfa1fad279d051dc666c8062f48deac89be32
ezito developer <33319787+ezitoir@users.noreply.github.com>
2023-03-22 03:13:54 -0700

    Update index.js

--- public/is/react/types/index.js
+++ public/is/react/types/index.js
@@ -1,4 +1,13 @@
 'use strict';
+/** @license React v16.13.1
+ * react-is.development.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 var hasSymbol = typeof Symbol === 'function' && Symbol.for;
 var REACT_ELEMENT_TYPE = hasSymbol ? Symbol.for('react.element') : 0xeac7;
 var REACT_PORTAL_TYPE = hasSymbol ? Symbol.for('react.portal') : 0xeaca;
