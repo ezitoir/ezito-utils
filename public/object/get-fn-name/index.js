6cc9ce6a7565828e5b8a1fdb165f69a942c57406
ezito developer <33319787+ezitoir@users.noreply.github.com>
2023-03-29 03:05:00 -0700

    Add files via upload

--- public/object/get-fn-name/index.js
+++ public/object/get-fn-name/index.js
@@ -1,14 +1,15 @@
 'use strict';
 const isFunction = require('ezito-utils/public/is/function');
-const makeError = require('ezito-utils/public/make-error');
+const isString   = require('ezito-utils/public/is/string');
+const makeError  = require('ezito-utils/public/make-error');
 
 module.exports = function getFunctionName(fn){
-    if(!isFunction(fn)) throw makeError(
+    if(!isFunction(fn) && !isString(fn)) throw makeError(
         '[PARAM-TYPE-ERROR]' ,
-        'fn param must be have function type' ,
+        'fn param must be have function | string type' ,
         1
     );
-    var pureFn = Function.prototype.toString.call(fn).slice('function'.length).trim();
+    var pureFn = isString(fn) ? fn : Function.prototype.toString.call(fn).slice('function'.length).trim();
     var name = pureFn.slice(0 , pureFn.indexOf('('));
     return name;
 }
