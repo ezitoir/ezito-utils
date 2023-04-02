4e588a432f69195929c26cf4b0d4fd03f2b3a190
ezito developer <33319787+ezitoir@users.noreply.github.com>
2023-04-02 05:01:57 -0700

    Add files via upload

---
+++ public/object/clone/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function cloneObject(obj) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = cloneObject(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = cloneObject(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+}
+
+if( typeof structuredClone === "function"){
+    module.exports = function (object , option){
+        try {
+            return structuredClone(object,option);
+        } catch (error) {
+            return cloneObject(object , option)
+        }
+    }
+}
+else {
+    module.exports = cloneObject;
+}
