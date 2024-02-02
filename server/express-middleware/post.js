2139f0b63e30906d99680c6c248446a84f5de73e
ezito developer <33319787+ezitoir@users.noreply.github.com>
2024-02-02 22:35:10 +0330

    Add files via upload

--- server/express-middleware/post.js
+++ server/express-middleware/post.js
@@ -1,9 +1,29 @@
+'use strict';
 
+const isFunction = require( "ezito-utils/public/is/function");
+const isHttp = require( "ezito-utils/public/is/http");
+const trace = require('ezito-utils/public/trace');
+const isArray = require( "ezito-utils/public/is/array");
 
-module.exports = function( fn ){
-    return function (req, res , next ){
+module.exports = function ( fn ){
+    const error = trace.createErrorOption(1,"parametr type error [fn] must be have function type")
+    if(!isFunction(fn)){
+        throw error;
+    }
+    
+    return function (req ,res ,next){
+        if(!isHttp.request(req)){
+            throw (trace.createErrorOption(1,"req param must be have http request type"))
+        }
+        if(!isHttp.response(res)){
+            throw (trace.createErrorOption(1,"res param must be have http response type"))
+        }
         const type = req.method.toLowerCase();
-        if(type !== 'post') return next(new Error({ status : 404 })); 
-        return fn.call( this , req ,res , next );
+        if(type !== 'post') {
+            if(isFunction(next)){
+                return next(new Error({ status : 405 })); 
+            }
+        }
+        return fn.call( this , req , res , next );
     }
 }
